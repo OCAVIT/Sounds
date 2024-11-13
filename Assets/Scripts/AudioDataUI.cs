7b8cb23b11be652ec78cdf54f96e56edfad4757c
OCAVIT <nikitastarostinn@yandex.com>
2024-11-14 01:21:28 +0300

    Completed
    Completed

--- Assets/Scripts/AudioDataUI.cs
+++ Assets/Scripts/AudioDataUI.cs
@@ -7,11 +7,20 @@ public class AudioDataUI : MonoBehaviour
     public AudioDataSO audioData;
     public Text uniqueIDText;
     public Text longText;
-    public GameObject audioListPanel;
+    public GameObject dangerousAudioListPanelLeft;
+    public GameObject dangerousAudioListPanelRight;
+    public GameObject friendlyAudioListPanelLeft;
+    public GameObject friendlyAudioListPanelRight;
+    public GameObject neutralAudioListPanelLeft;
+    public GameObject neutralAudioListPanelRight;
     public GameObject longTextPanel;
+    public GameObject audiolistpanel;
     public Button showListButton;
     public Button showTextButton;
     public Button hideAllButton;
+    public GameObject audioItemPrefab;
+    public AudioSource audioSource;
+    public Slider volumeSlider;
 
     void Start()
     {
@@ -19,37 +28,95 @@ public class AudioDataUI : MonoBehaviour
         showListButton.onClick.AddListener(ShowList);
         showTextButton.onClick.AddListener(ShowText);
         hideAllButton.onClick.AddListener(HideAll);
+        volumeSlider.value = audioSource.volume;
+        volumeSlider.onValueChanged.AddListener(ChangeVolume);
+
+        if (audioData != null && uniqueIDText != null)
+        {
+            Debug.Log("Setting text: " + audioData.uniqueID);
+            uniqueIDText.text = audioData.uniqueID;
+        }
+        else
+        {
+            Debug.LogError("AudioData or Text component is not set.");
+        }
+    }
+
+    void ChangeVolume(float value)
+    {
+        audioSource.volume = value;
     }
 
     void ShowList()
     {
-        audioListPanel.SetActive(true);
+        audiolistpanel.SetActive(true);
+        dangerousAudioListPanelLeft.SetActive(true);
+        dangerousAudioListPanelRight.SetActive(true);
+        friendlyAudioListPanelLeft.SetActive(true);
+        friendlyAudioListPanelRight.SetActive(true);
+        neutralAudioListPanelLeft.SetActive(true);
+        neutralAudioListPanelRight.SetActive(true);
         longTextPanel.SetActive(false);
 
-        List<AudioClipData> currentList = null;
-        switch (audioData.audioContentType)
+        PopulateAudioList(dangerousAudioListPanelLeft, dangerousAudioListPanelRight, audioData.dangerousAudioClips);
+        PopulateAudioList(friendlyAudioListPanelLeft, friendlyAudioListPanelRight, audioData.friendlyAudioClips);
+        PopulateAudioList(neutralAudioListPanelLeft, neutralAudioListPanelRight, audioData.neutralAudioClips);
+    }
+
+    void PopulateAudioList(GameObject panelLeft, GameObject panelRight, List<AudioClipData> audioClips)
+    {
+        foreach (Transform child in panelLeft.transform)
         {
-            case AudioContentType.Dangerous:
-                currentList = audioData.dangerousAudioClips;
-                break;
-            case AudioContentType.Friendly:
-                currentList = audioData.friendlyAudioClips;
-                break;
-            case AudioContentType.Neutral:
-                currentList = audioData.neutralAudioClips;
-                break;
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in panelRight.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < audioClips.Count; i++)
+        {
+            GameObject audioItem = Instantiate(audioItemPrefab, i % 2 == 0 ? panelLeft.transform : panelRight.transform);
+            Text clipNameText = audioItem.GetComponentInChildren<Text>();
+            Button playButton = audioItem.GetComponentInChildren<Button>();
+
+            clipNameText.text = audioClips[i].audioClip.name;
+
+            int index = i;
+            playButton.onClick.AddListener(() => PlayAudioClip(audioClips[index].audioClip));
         }
     }
 
     void ShowText()
     {
-        audioListPanel.SetActive(false);
+        dangerousAudioListPanelLeft.SetActive(false);
+        dangerousAudioListPanelRight.SetActive(false);
+        friendlyAudioListPanelLeft.SetActive(false);
+        friendlyAudioListPanelRight.SetActive(false);
+        neutralAudioListPanelLeft.SetActive(false);
+        neutralAudioListPanelRight.SetActive(false);
         longTextPanel.SetActive(true);
+        audiolistpanel.SetActive(false);
     }
 
     void HideAll()
     {
-        audioListPanel.SetActive(false);
+        dangerousAudioListPanelLeft.SetActive(false);
+        dangerousAudioListPanelRight.SetActive(false);
+        friendlyAudioListPanelLeft.SetActive(false);
+        friendlyAudioListPanelRight.SetActive(false);
+        neutralAudioListPanelLeft.SetActive(false);
+        neutralAudioListPanelRight.SetActive(false);
         longTextPanel.SetActive(false);
+        audiolistpanel.SetActive(false);
+    }
+
+    void PlayAudioClip(AudioClip clip)
+    {
+        if (audioSource != null)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
     }
 }
